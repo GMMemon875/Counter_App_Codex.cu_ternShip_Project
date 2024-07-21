3f52c5c31301e6379ea406f31ac4752b1fbd2afe
GMMemon875 <ghulammemon875@gmail.com>
2024-07-21 13:13:20 +0500

    Simply Counter App

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,14 @@ function App() {
         <div className=" card">
           <h1 className="Apptext">Counter app</h1>
           {/* <p>Current Count</p> */}
+          {/* use state men save keya he gaya count men 0  */}
           <h1>{count}</h1>
+
+          {/* on click per usstate wala counter increment button per count + 1 ho jaie  */}
           <button onClick={() => setCount(count + 1)}>Increment</button>
+          {/* on click per usstate wala counter Dicrements button per count - 1 ho jaie  */}
           <button onClick={() => setCount(count - 1)}>Dicrement</button>
+          {/* Reset button per onclick per == "" empty call ho jo be number incress hoie hen wo 0 ho jaie  */}
           <button className="btnReset" onClick={() => setCount(count == "")}>
             {" "}
             Reset
